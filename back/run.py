0869b2847787005614e3e04687af4ecfb6f27750
sdhuen01-del <sdhuen01@gmail.com>
2026-07-02 11:42:30 +0900

    0702

--- back/run.py
+++ back/run.py
@@ -1,3 +1,4 @@
+import os
 import eventlet
 eventlet.monkey_patch()
 
@@ -9,4 +10,6 @@ from app import create_app, socketio
 app = create_app()
 
 if __name__ == '__main__':
-    socketio.run(app, host='0.0.0.0', port=5000, debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    debug = os.environ.get('FLASK_ENV') == 'development'
+    socketio.run(app, host='0.0.0.0', port=port, debug=debug)
